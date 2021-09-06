84d9eccd24e6d9d7fe0b4184e71605ab9b5b1264
Blake Nuttall <blakedyblake@gmail.com>
2021-09-06 11:42:26 -0600

    data object on server

---
+++ Pages/main.js
@@ -0,0 +1,7 @@
+//const { default: axios } = require("axios");
+
+//const { response } = require("express")
+
+axios.get('/races',()=>{
+    console.log(response.data)
+})
--- Server/server.js
+++ Server/server.js
@@ -2,10 +2,37 @@ const express = require('express')
 const cors = require('cors')
 const path = require('path')
 const app = express()
+const races = {
+    "Elf":{
+
+    },
+    "Dwarf":{
+
+    },
+    "Human":{}
+}
+//require('./Databases/races.js')
 
 app.use(express.json())
 app.use(cors())
 
+
+app.get('/', (req,res)=>{
+    res.sendFile(path.join(__dirname,'../Pages/main.html'))
+})
+app.get('/css', (req,res)=>{
+    res.sendFile(path.join(__dirname,'../Style/main.css'))
+})
+app.get('/js', (req,res)=>{
+    res.sendFile(path.join(__dirname, '../Pages/main.js'))
+})
+
+app.use('/css', express.static(path.join(__dirname, '../Style/main.css')))
+app.use('/js', express.static(path.join(__dirname, '../Pages/main.js')))
+
+app.get('/races',(req,res)=>{
+    res.status(200).send(races)
+})
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, ()=>{
