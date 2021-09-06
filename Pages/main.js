31d726d2e8a01e6499b145de09386626f33ef25b
Blake Nuttall <blakedyblake@gmail.com>
2021-09-06 11:58:18 -0600

    script js for html

--- Pages/main.js
+++ Pages/main.js
@@ -1,8 +1,8 @@
 // const { default: axios } = require("axios");
 
 // const { response } = require("express")
-const baseURL = process.env.baseURL || "http://localhost:4000"
-console.log(baseURL);
+// const baseURL = process.env.baseURL || "http://localhost:4000"
+// console.log(baseURL);
 // axios.get(`${baseURL}/races`,(response)=>{
 //     console.log(response.data)
 // })
